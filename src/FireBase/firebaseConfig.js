d3c8af065982ec880d5bdba023a0f169db1438f9
Mohamed Abodief <mohamed.abodeaf@gmail.com>
2025-06-26 22:59:46 +0300

    add .env file

--- src/FireBase/firebaseConfig.js
+++ src/FireBase/firebaseConfig.js
@@ -2,26 +2,17 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBZDFrAARgCTXF_L5KFqD_EOQm_5nF_uTg',
-  authDomain: 'smsark-alaqary.firebaseapp.com',
-  projectId: 'smsark-alaqary',
-  storageBucket: 'smsark-alaqary.firebasestorage.app',
-  messagingSenderId: '165621685338',
-  appId: '1:165621685338:web:295441459d4d5443e9cc63',
-  measurementId: 'G-0ZMCMWXZ1X',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-
 export const auth = getAuth(app);
-
