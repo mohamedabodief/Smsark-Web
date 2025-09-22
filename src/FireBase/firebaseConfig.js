7983d320861363129ee7e0ebe7dab4f148ebccff
Mohamed Abodief <mohamed.abodeaf@gmail.com>
2025-09-23 02:38:44 +0300

    edit error

--- src/FireBase/firebaseConfig.js
+++ src/FireBase/firebaseConfig.js
@@ -1,35 +1,3 @@
-// // src/firebaseConfig.js
-
-// import { initializeApp } from 'firebase/app';
-// import { getAuth } from 'firebase/auth';
-// import { getFirestore, Timestamp } from 'firebase/firestore';
-// import { getMessaging, getToken, onMessage } from 'firebase/messaging';
-// import { getStorage } from "firebase/storage";
-// const storage = getStorage(app);
-// export { storage };
-
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-// };
-
-// // ✅ تهيئة التطبيق
-// export const app = initializeApp(firebaseConfig);
-
-// // ✅ خدمات Firebase
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
-// export const timestamp = Timestamp;
-
-// // ✅ FCM (Firebase Cloud Messaging)
-// export const messaging = getMessaging(app);
-// export { getToken, onMessage };
-
 // src/firebaseConfig.js
 
 import { initializeApp } from 'firebase/app';
@@ -37,6 +5,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore, Timestamp } from 'firebase/firestore';
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 import { getStorage } from 'firebase/storage';
+
 // ✅ config
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -47,7 +16,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-// console.log('🔥 Firebase API Key:', import.meta.env.VITE_FIREBASE_API_KEY);
 
 // ✅ initialize app
 export const app = initializeApp(firebaseConfig);
@@ -60,3 +28,8 @@ export const messaging = getMessaging(app);
 export const storage = getStorage(app);
 export { getToken, onMessage };
 
+// ✅ helper function لتصحيح أي لينك صور من Storage
+export function fixStorageUrl(url) {
+  if (!url) return url;
+  return url.replace('firebasestorage.app', 'appspot.com');
+}
